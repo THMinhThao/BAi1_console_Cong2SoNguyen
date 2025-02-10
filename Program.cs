82309faaa812eca145850f7aa4b2f1efeef38386
THMINHTHAO <thaominh245@gmail.com>
2025-02-10 09:28:55 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Nhập họ và tên");
+        string ten = Console.ReadLine();
+        Console.Write("Chào bạn {0} :"+ ten );
+        
+
+        /*
+        Console.WriteLine("Nhập số thứ nhất:"); 
+        double soThuNhat = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Nhập số thứ hai:");
+        double soThuHai = Convert.ToDouble(Console.ReadLine());
+        double tong = soThuNhat + soThuHai;
+        Console.WriteLine(" Tổng của hai số là: " + tong);
+        Console.WriteLine(soThuNhat + "+" + soThuHai + "=" + tong);
+        Console.WriteLine(" {0} + {1} = {2} ", soThuNhat,soThuHai,tong);
+        */
+    }
+}
